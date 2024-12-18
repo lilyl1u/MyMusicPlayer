e56cd3735cea59d19b090ec34632dc4d363af60d
lily liu <lilyliu438@gmail.com>
2024-12-17 19:05:14 -0500

    added exit confirmation and volume trackbar

--- MyMusicPlayer/Form1.Designer.cs
+++ MyMusicPlayer/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             Player = new AxWMPLib.AxWindowsMediaPlayer();
             textBox1 = new TextBox();
@@ -37,7 +38,10 @@
             button4 = new Button();
             button5 = new Button();
             openFileDialog1 = new OpenFileDialog();
+            trackBar1 = new TrackBar();
+            timer2 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)Player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
             // Player
@@ -48,6 +52,7 @@
             Player.OcxState = (AxHost.State)resources.GetObject("Player.OcxState");
             Player.Size = new Size(684, 182);
             Player.TabIndex = 0;
+            Player.Enter += Player_Enter;
             // 
             // textBox1
             // 
@@ -56,6 +61,7 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(115, 33);
             textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
@@ -63,9 +69,9 @@
             button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.InfoText;
-            button1.Location = new Point(27, 194);
+            button1.Location = new Point(12, 194);
             button1.Name = "button1";
-            button1.Size = new Size(97, 44);
+            button1.Size = new Size(71, 44);
             button1.TabIndex = 2;
             button1.Text = "Browse";
             button1.UseVisualStyleBackColor = false;
@@ -77,7 +83,7 @@
             button2.FlatStyle = FlatStyle.Popup;
             button2.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button2.ForeColor = SystemColors.InfoText;
-            button2.Location = new Point(554, 194);
+            button2.Location = new Point(570, 194);
             button2.Name = "button2";
             button2.Size = new Size(97, 44);
             button2.TabIndex = 3;
@@ -91,7 +97,7 @@
             button3.FlatStyle = FlatStyle.Popup;
             button3.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button3.ForeColor = SystemColors.InfoText;
-            button3.Location = new Point(422, 194);
+            button3.Location = new Point(467, 194);
             button3.Name = "button3";
             button3.Size = new Size(97, 44);
             button3.TabIndex = 4;
@@ -105,9 +111,9 @@
             button4.FlatStyle = FlatStyle.Popup;
             button4.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button4.ForeColor = SystemColors.InfoText;
-            button4.Location = new Point(290, 194);
+            button4.Location = new Point(164, 194);
             button4.Name = "button4";
-            button4.Size = new Size(97, 44);
+            button4.Size = new Size(72, 44);
             button4.TabIndex = 5;
             button4.Text = "Pause";
             button4.UseVisualStyleBackColor = false;
@@ -119,9 +125,9 @@
             button5.FlatStyle = FlatStyle.Popup;
             button5.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button5.ForeColor = SystemColors.InfoText;
-            button5.Location = new Point(157, 194);
+            button5.Location = new Point(89, 194);
             button5.Name = "button5";
-            button5.Size = new Size(97, 44);
+            button5.Size = new Size(69, 44);
             button5.TabIndex = 6;
             button5.Text = "Play";
             button5.UseVisualStyleBackColor = false;
@@ -132,12 +138,28 @@
             openFileDialog1.FileName = "openFileDialog1";
             openFileDialog1.Multiselect = true;
             // 
+            // trackBar1
+            // 
+            trackBar1.AutoSize = false;
+            trackBar1.BackColor = Color.Thistle;
+            trackBar1.Location = new Point(133, 149);
+            trackBar1.Margin = new Padding(1);
+            trackBar1.Maximum = 100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(144, 33);
+            trackBar1.TabIndex = 7;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // timer2
+            // 
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.AntiqueWhite;
             ClientSize = new Size(679, 250);
+            Controls.Add(trackBar1);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
@@ -151,6 +173,7 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "My Music Player";
             ((System.ComponentModel.ISupportInitialize)Player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -165,5 +188,8 @@
         private Button button4;
         private Button button5;
         private OpenFileDialog openFileDialog1;
+        private TrackBar trackBar1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
--- MyMusicPlayer/Form1.cs
+++ MyMusicPlayer/Form1.cs
@@ -1,3 +1,4 @@
+using System.IO;
 namespace MyMusicPlayer
 {
     public partial class Form1 : Form
@@ -18,7 +19,12 @@ namespace MyMusicPlayer
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Player.Ctlcontrols.play(); //plays music
+            if (!string.IsNullOrEmpty(openFileDialog1.FileName)){
+                Player.URL = openFileDialog1.FileName;
+                Player.Ctlcontrols.play();// plays music
+
+            }
+            //Player.Ctlcontrols.play(); //plays music
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -29,11 +35,32 @@ namespace MyMusicPlayer
         private void button3_Click(object sender, EventArgs e)
         {
             Player.Ctlcontrols.stop(); //stop music
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Close();
+            Player.Ctlcontrols.stop();
+            if (MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
         }
+
+        private void Player_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Player.settings.volume = trackBar1.Value;
+        }
+
     }
 }
